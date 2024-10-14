3743d09411cc689f6fe74b0efcce1e2993f8f648
Esteban Hernandez <joehv33@gmail.com>
2024-10-13 21:55:56 -0500

    Terminado y validado

--- src/ExtraPrograma.java
+++ src/ExtraPrograma.java
@@ -2,4 +2,182 @@ import java.util.Scanner;
 
 public class ExtraPrograma {
 
+    Scanner leer = new Scanner(System.in);
+    boolean ascendente;
+    boolean logs;
+    int x, y;
+
+    public void menuOrden(){
+        System.out.println("Menu de Orden:");
+        System.out.println("1. Ascendente");
+        System.out.println("2. Descendente");
+        x = validacion(leer, "Escoja una opcion: ", true);
+        
+        switch (x) {
+            case 1:
+                ascendente = true;
+                break;
+        
+            case 2:
+                ascendente = false;
+                break;
+
+            default:
+                System.out.println("Opcion ingresada no valida :/");
+                break;
+        }
+    }
+
+    public void menuLogs(){
+        System.out.println("¿Imprimir pasos?");
+        System.out.println("1. Si");
+        System.out.println("2. No");
+        y = validacion(leer, "Escoja una opcion: ", true);
+
+        switch (y) {
+            case 1:
+                logs = true;
+                break;
+        
+            case 2:
+                logs = false;
+                break;
+
+            default:
+                System.out.println("Opcion ingresada no valida :/");
+                break;
+        }
+    }
+
+    public void menuGeneral(){
+        
+        Scanner leer = new Scanner(System.in);
+        MetodosOrdenamiento mO = new MetodosOrdenamiento();
+        ExtraPrograma eP = new ExtraPrograma();
+
+        int [] arreglo = null;
+        int [] arregloOriginal = null;
+        int w = 1, x, y;
+        boolean ordenado = false;
+
+            do{
+                System.out.println("Menu principal: ");
+                System.out.println("1. Ingresar un arreglo");
+                System.out.println("2. Ordenar arreglo");
+                System.out.println("0. Salir");
+                w = validacion(leer, "Escoja una opcion: ", true);
+        
+                switch (w) {
+                    case 1:
+                        int lengthArray = validacion(leer, "Ingrese el tamaño del arreglo (entero positivo): ", false);
+                        arreglo = new int[lengthArray];
+
+                        for (int i = 0; i < lengthArray; i++){
+                           arreglo[i] = validacion(leer, "Ingrese el numero en la posicion " + (i+1) + " del arreglo: ", true);
+                        }
+                        arregloOriginal = arreglo.clone(); 
+                        ordenado = false;
+                        System.out.println( "El arreglo creado es: " + java.util.Arrays.toString(arreglo));
+
+                    break;
+        
+                    case 2:
+                        if (arreglo == null){
+                            System.out.println("Todavia no se ha ingresado un arreglo " + 
+                            "para poder ingresar un arreglo seleccione la opcion 1");
+                        } else {
+
+
+                            do {
+
+                                if (ordenado){
+                                    System.out.println("El arreglo ya esta ordenado");
+                                    System.out.println("¿Desea reordenarlo?");
+                                    System.out.println("1. Si");
+                                    System.out.println("2. No");
+                                    y = validacion(leer, "Escoja una opcion: ", true);
+                                    if (y == 2){
+                                        break;
+                                    } 
+                                    arreglo = arregloOriginal.clone(); 
+                                }
+
+                                System.out.println("Metodos de ordenamiento:");
+                                System.out.println("1. Metodo burbuja");
+                                System.out.println("2. Metodo Seleccion");
+                                System.out.println("3. Metodo Insercion");
+                                System.out.println("4. Metodo Burbuja Mejorado");
+                                System.out.println("0. Volver al Menu principal");
+                                x = validacion(leer, "Escoja una opcion: ", true);
+
+                                switch (x) {
+                                case 1:
+                                    eP.menuOrden();
+                                    eP.menuLogs();
+                                    mO.sortBubble(arreglo, eP.ascendente, eP.logs);
+                                    System.out.println( "El arreglo ordenado por Burbuja: " + java.util.Arrays.toString(arreglo));
+                                    ordenado = true;
+                                    break;
+                            
+                                case 2:
+                                    eP.menuOrden();
+                                    eP.menuLogs();
+                                    mO.sortSelection(arreglo, eP.ascendente, eP.logs);
+                                    System.out.println( "El arreglo ordenado por Seleccion: " + java.util.Arrays.toString(arreglo));
+                                    ordenado = true;
+                                    break;
+        
+                                case 3:
+                                    eP.menuOrden();
+                                    eP.menuLogs();
+                                    mO.sortInsertion(arreglo, eP.ascendente, eP.logs);
+                                    System.out.println( "El arreglo ordenado por Insercion : " + java.util.Arrays.toString(arreglo));
+                                    ordenado = true;
+                                    break;
+                                
+                                case 4:
+                                    eP.menuOrden();
+                                    eP.menuLogs();
+                                    mO.sortBubbleAdvance(arreglo, eP.ascendente, eP.logs);
+                                    System.out.println( "El arreglo ordenado por Burbuja Mejorado: " + java.util.Arrays.toString(arreglo));
+                                    ordenado = true;
+                                    break;
+        
+                                default:
+                                    if (x != 0){
+                                        System.out.println("Opcion ingresada invalida :/");
+                                    }
+                                }
+                            } while (x != 0);
+                        }
+                    break;
+                    default:
+                        if (w != 0){
+                            System.out.println("La opcion ingresada no es valida :/");
+                        }
+                }
+            } while (w != 0); 
+    }
+
+    public static int validacion(Scanner leer, String mensaje, boolean permitirNegativos){
+            
+            int arrayLength = 0;
+    
+            do{
+                System.out.print(mensaje);
+                while (!leer.hasNextInt()) {
+                    System.out.println("Debes ingresar un entero valido :/");
+                    System.out.print(mensaje);
+                    leer.next();
+                }
+                arrayLength = leer.nextInt();
+                if (!permitirNegativos && arrayLength <= 0){
+                    System.out.println("El tamaño debe ser entero positivo mayor a 0 :/");
+                }
+            } while(! permitirNegativos && arrayLength <= 0);
+            return arrayLength;
+        
+    }
+    
+
 }
--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -1,71 +1,119 @@
 import java.util.Arrays;
 
 public class MetodosOrdenamiento {
-    public int[] sortBubble(int[] arreglo, boolean ascendente, boolean logs){
+    ExtraPrograma extra = new ExtraPrograma();
+
+    // LISTO
+    public int[] sortBubble(int[] arregloOriginal, boolean ascendente, boolean logs){
+        int lengthArray = arregloOriginal.length;
+
+            for (int i = 0; i < lengthArray; i++){
+                for (int j = i + 1; j < lengthArray; j++){
+                    if (logs){
+                        System.out.println("i = " + i + "  j = " + j + "  [i] = " + arregloOriginal[i] + "  [j] = " + arregloOriginal[j]);
+                    }
+                    if (ascendente){
+                        if (arregloOriginal[j] < arregloOriginal[i]){
+                            if (logs) {
+                                System.out.println("\tIntercambiamos " + arregloOriginal[j] + " con " + arregloOriginal[i + 1]);
+                            }
+                            int aux = arregloOriginal[i];
+                            arregloOriginal[i] = arregloOriginal[j];
+                            arregloOriginal[j] = aux;
+                            if (logs) {
+                                System.out.println("\t--------" + Arrays.toString(arregloOriginal));
+                            }
+                        }
+                    } else {
+                        if (arregloOriginal[j] > arregloOriginal[i]){
+                            if (logs) {
+                                System.out.println("\tIntercambiamos " + arregloOriginal[j] + " con " + arregloOriginal[i + 1]);
+                            }
+                            int aux = arregloOriginal[i];
+                            arregloOriginal[i] = arregloOriginal[j];
+                            arregloOriginal[j] = aux;
+                            if (logs) {
+                                System.out.println("\t--------" + Arrays.toString(arregloOriginal));
+                            }
+                        }
+                    }
+                }
+            }
+        return arregloOriginal;
+    }
+
+    // LISTO
+    public int[] sortSelection(int[] arreglo, boolean ascendente, boolean logs){
         int lengthArray = arreglo.length;
 
-        for (int i = 0; i < lengthArray; i++){
-            for (int j = i + 1; j < lengthArray; j++){
-
-                if (ascendente){
-                    if (arreglo[j] < arreglo[i]){
-                        int aux = arreglo[i];
-                        arreglo[i] = arreglo[j];
-                        arreglo[j] = aux;
-                    }
-                } else {
-                    if (arreglo[j] > arreglo[i]){
-                        int aux = arreglo[i];
-                        arreglo[i] = arreglo[j];
-                        arreglo[j] = aux;
-                    }
-                }
-
-            }
-        }
-        return arreglo;
-    }
-
-    public int[] sortSelection(int[] arregloOriginal, boolean ascendente, boolean logs) {
-
-        int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
-
         if (ascendente){
-            for (int i = 0; i < arreglo.length - 1; i++) {
-                int minimo = i;
-                for (int j = i + 1; j < arreglo.length; j++) {
-                    if (arreglo[j] < arreglo[minimo]) {
-                        minimo = j;
+            for ( int i = 0; i < lengthArray; i++){
+                int indice = i;
+    
+                for (int j = i + 1; j < lengthArray; j++){
+                    if (logs) {
+                        System.out.println("i = " + i + "  j = " + j + "  [indice] = " + arreglo[indice] + "  [j] = " + arreglo[j]);
+                    }
+                    if (arreglo[j] < arreglo[indice]){
+                        indice = j;
                     }
                 }
-                int menor = arreglo[minimo];
-                arreglo[minimo] = arreglo[i];
-                arreglo[i] = menor;
+                if (logs) {
+                    System.out.println("\tIntercambiamos " + arreglo[i] + " con " + arreglo[indice]);
+                }
+                // Intercambio de indice
+                int aux = arreglo[indice];
+                arreglo[indice] = arreglo[i];
+                arreglo[i] = aux;
+                if (logs) {
+                    System.out.println("\t--------" + Arrays.toString(arreglo));
+                }
             }
         } else {
-            for (int i = 0; i < arreglo.length - 1; i++) {
-                int minimo = i;
-                for (int j = i + 1; j < arreglo.length; j++) {
-                    if (arreglo[j] > arreglo[minimo]) {
-                        minimo = j;
+            for ( int i = 0; i < lengthArray; i++){
+                int indice = i;
+    
+                for (int j = i + 1; j < lengthArray; j++){
+                    if (logs) {
+                        System.out.println("i = " + i + "  j = " + j + "  [indice] = " + arreglo[indice] + "  [j] = " + arreglo[j]);
+                    }
+                    if (arreglo[j] > arreglo[indice]){
+                        indice = j;
                     }
                 }
-                int menor = arreglo[minimo];
-                arreglo[minimo] = arreglo[i];
-                arreglo[i] = menor;
+                if (logs) {
+                    System.out.println("\tIntercambiamos " + arreglo[i] + " con " + arreglo[indice]);
+                }
+                // Intercambio de indice
+                int aux = arreglo[indice];
+                arreglo[indice] = arreglo[i];
+                arreglo[i] = aux;
+                if (logs) {
+                    System.out.println("\t--------" + Arrays.toString(arreglo));
+                }
             }
         }
         return arreglo;
     }
 
+    // LISTO 
     public int[] sortInsertion(int[] arreglo, boolean ascendente, boolean logs){
         if (ascendente){
             for (int i = 1; i < arreglo.length; i++){
                 int aux = arreglo[i];
-                int j = i - 1;           
+                int j = i - 1;   
+                if (logs) {
+                    System.out.println("i = " + i + "  j = " + j + "  [aux] = " + aux);
+                }        
                 while (j >= 0 && arreglo[j] > aux) {
+                    if (logs) {
+                        System.out.println("\tComparando " + aux + " con " + arreglo[j]);
+                    }
                     arreglo[j + 1] = arreglo[j];
                     j--;
+                    if (logs) {
+                        System.out.println("\t--------" + Arrays.toString(arreglo));
+                    }
                 }
                 arreglo[j + 1] = aux;
             }
@@ -73,9 +121,18 @@ public class MetodosOrdenamiento {
             for (int i = 1; i < arreglo.length; i++){
                 int aux = arreglo[i];
                 int j = i - 1;           
-                while (j >= 0 && arreglo[j] > aux) {
+                if (logs) {
+                    System.out.println("i = " + i + "  j = " + j + "  [aux] = " + aux);
+                } 
+                while (i > 0 && arreglo[i] < aux) {
+                    if (logs) {
+                        System.out.println("\tComparando " + aux + " con " + arreglo[j]);
+                    }
                     arreglo[j + 1] = arreglo[j];
-                    j--;
+                    j++;
+                    if (logs) {
+                        System.out.println("\t--------" + Arrays.toString(arreglo));
+                    }
                 }
                 arreglo[j + 1] = aux;
             }
@@ -83,46 +140,67 @@ public class MetodosOrdenamiento {
         return arreglo;
     }
 
+    // LISTO
     public int [] sortBubbleAdvance(int[] arreglo, boolean ascendente, boolean logs){
 
         int n = arreglo.length;
         boolean intercambio = false;
 
-        if (ascendente){
-            for (int i = 0; i < n; i++){
-            intercambio = false;
-                for (int j = 0; j < n-1-i; j++){
-                    if (arreglo[j] > arreglo[j + 1]){
-                //Intrecambio 
-                    int aux = arreglo[j];
-                    arreglo[j] = arreglo[j + 1];
-                    arreglo[j + 1] = aux;
-                    intercambio = true;
-                    }
-                }
-                //Si no hubo intercambio
-                if (!intercambio){
-                    break;
-                }
-            }
-        } else {
-            for (int i = 0; i < n; i++){
+            if (ascendente){
+                for (int i = 0; i < n; i++){
+                System.out.println("Pasada "+ i);
                 intercambio = false;
-                for (int j = 0; j < n-1-i; j++){
-                    if (arreglo[j] > arreglo[j + 1]){
+                    for (int j = 0; j < n-1-i; j++){
+                        if (logs) {
+                            System.out.println(" i = " + i + "  j = " + j + "  [i] = " + arreglo[i] + "  [j] = " + arreglo[j]);
+                        }
+                        if (arreglo[j] > arreglo[j + 1]){
+                            if (logs) {
+                                System.out.println("\tIntercambiamos " + arreglo[j] + " con " + arreglo[i + 1]);
+                            }
                     //Intrecambio 
                         int aux = arreglo[j];
                         arreglo[j] = arreglo[j + 1];
                         arreglo[j + 1] = aux;
                         intercambio = true;
+                        if (logs) {
+                            System.out.println("\t--------" + Arrays.toString(arreglo));
+                        }
+                        }
                     }
-                }
                     //Si no hubo intercambio
                     if (!intercambio){
                         break;
                     }
+                }
+            } else {
+                for (int i = 0; i < n; i++){
+                    System.out.println("Pasada "+ i);
+                    intercambio = false;
+                    for (int j = 0; j < n-1-i; j++){
+                        if (logs) {
+                            System.out.println(" i = " + i + "  j = " + j + "  [i] = " + arreglo[i] + "  [j] = " + arreglo[j]);
+                        }
+                        if (arreglo[j] < arreglo[j + 1]){
+                            if (logs) {
+                                System.out.println("\tIntercambiamos " + arreglo[j] + " con " + arreglo[i + 1]);
+                            }
+                        //Intrecambio 
+                            int aux = arreglo[j];
+                            arreglo[j] = arreglo[j + 1];
+                            arreglo[j + 1] = aux;
+                            intercambio = true;
+                            if (logs) {
+                                System.out.println("\t--------" + Arrays.toString(arreglo));
+                            }
+                        }
+                    }
+                        //Si no hubo intercambio
+                        if (!intercambio){
+                            break;
+                        }
+                }
             }
-        }
     return arreglo;
     }
 }
